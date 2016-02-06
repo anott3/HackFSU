1c946b072abc9dd524093ea64d8b2876013f3962
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-02-06 15:55:21 -0500

    Added buffer

--- node-js-getting-started/routes/index.js
+++ node-js-getting-started/routes/index.js
@@ -1,26 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var instruction = '';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/obtainInstruction', function(req, res) {
+  res.send(instruction);
   //res.send('A get request made');
 });
 
 router.post('/up', function(req, res){
-	res.send('Up Sent');
+	instruction = instruction + 'U';
+	res.send(instruction);
 });
 
 router.post('/down', function(req, res){
-	res.send('Down Sent');
+	instruction = instruction + 'D';
+	res.send(instruction);
 });
 
 router.post('/left', function(req, res){
-	res.send('Left Sent');
+	instruction = instruction + 'L';
+	res.send(instruction);
 });
 
 router.post('/right', function(req, res){
-	res.send('Right Sent');
+	instruction = instruction + 'R';
+	res.send(instruction);
 });
 
 module.exports = router;
