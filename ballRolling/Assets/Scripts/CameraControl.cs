1f6e8cf0bce6868ccab41fd31a144d0ff06f98fd
manav95 <manavbiodutta@yahoo.com>
2016-02-06 18:20:53 -0500

    -m :-nmnmnm:

--- ballRolling/Assets/Scripts/CameraControl.cs
+++ ballRolling/Assets/Scripts/CameraControl.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-	public GameObject player;
+	private GameObject player;
 	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
+		player = GameObject.FindWithTag ("Player");
 		offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = offset + player.transform.position;
+		offset = offset + player.transform.position;
 	}
 }
